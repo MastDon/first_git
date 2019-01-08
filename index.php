99818245be12575c225625d657d44ec8ccc4c647
MastDon <sid2@yandex.ru>
2019-01-08 15:59:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
-<?php
-
-function getMyName() {
-
-    $a = 50;
-
-    if ($a < 100 ){
-        $a++;
-        echo ($a);
-    }
-}
-
-echo ('Git' + getMyName());
-
-?>
-
 <html>
 
 <h1>Hello Git</h1>
